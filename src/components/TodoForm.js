16c2f6c1433b4c0ee8ee04d4cae0a79f753ac149
Stephanie Cambria <107421370+StephCambria@users.noreply.github.com>
2023-01-18 11:49:18 -0500

    console logging input

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -10,10 +10,13 @@ function TodoForm(props) {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    //   props.onSubmit({
-    //     id: Math.floor(Math.random() * 10000),
-    //     text: input,
-    //   });
+    // ====================================================
+    // Using props and Math.random to create a random ID for each input
+    props.onSubmit({
+      id: Math.floor(Math.random() * 10000),
+      text: input,
+    });
+    // ====================================================
     setInput("");
   };
 
