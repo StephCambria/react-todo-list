add1cfe46e3800693785b487e7cf1ca06fba8022
Stephanie Cambria <107421370+StephCambria@users.noreply.github.com>
2023-01-18 12:27:46 -0500

    remove todo working

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,22 +1,40 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import TodoForm from "./TodoForm";
+import Todo from "./Todo";
 
 export const TodoList = () => {
-    const [todos, setTodos] = useState([]);
-    const addTodo = todo => {
-        if (!todo.text || /^\s*$/.test(todo.text)) {
-            return;
-        }
-        const newTodos = [todo, ...todos];
-        setTodos(newTodos);
-        console.log(...todos);
+  const [todos, setTodos] = useState([]);
+  const addTodo = (todo) => {
+    if (!todo.text || /^\s*$/.test(todo.text)) {
+      return;
     }
+    const newTodos = [todo, ...todos];
+    setTodos(newTodos);
+    console.log(...todos);
+  };
+
+  const removeTodo = (id) => {
+    const removeArr = [...todos].filter((todo) => todo.id !== id);
+    setTodos(removeArr);
+  };
+
+  const completeTodo = (id) => {
+    let updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.isComplete = !todo.isComplete;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+
   return (
     <div>
-        <h1>What the McShit?</h1>
-        <TodoForm onSubmit={addTodo}/>
+      <h1>What the McShit?</h1>
+      <TodoForm onSubmit={addTodo} />
+      <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo} />
     </div>
-  )
-}
+  );
+};
 
 export default TodoList;
